754986fa31a71828288a65b22d44e06d6b0cd287
leomono35 <55753054+leomono35@users.noreply.github.com>
2019-12-11 19:17:11 +0100

    Create Plateau.java

---
+++ Plateau.java
@@ -0,0 +1,22 @@
+public class Plateau {
+
+    protected char image;
+
+    protected int ressource;
+
+    protected String imageRessource;
+
+    protected int nbrRessource;
+
+    public Plateau(char n_image, int n_ressource, String n_imageRessource) {
+        this.image=n_image;
+        this.imageRessource=n_imageRessource;
+        this.ressource=n_ressource;
+        this.nbrRessource=0;
+    }
+
+    public void afficherCarte(int type) {
+
+    }
+
+}
