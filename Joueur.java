fe18a7fb487edbfa84bb3eef2a87b2d4203ce4ac
leomono35 <55753054+leomono35@users.noreply.github.com>
2019-12-11 19:16:57 +0100

    Create Joueur.java

---
+++ Joueur.java
@@ -0,0 +1,26 @@
+public class Joueur {
+
+    protected int id;
+
+    protected Plateau plateau;
+
+    protected SousListe sousListe;
+
+    protected int gold;
+
+    public Joueur(int n_id, Plateau n_plateau, SousListe n_sousListe) {
+        this.id=n_id;
+        this.plateau=n_plateau;
+        this.sousListe=n_sousListe;
+        this.gold=3;
+    }
+
+    public void showSousListe() {
+
+    }
+
+    public void hideSousListe() {
+
+    }
+
+}
