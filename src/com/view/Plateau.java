39f951565c6d8671359575ac81f5b049edb056a2
Ines Melounou <imelouno@utbm.fr>
2019-12-20 19:02:14 +0100

    ok : label carte et gold + placement grid bien dessiné

--- src/com/view/Plateau.java
+++ src/com/view/Plateau.java
@@ -1,18 +1,24 @@
 package com.view;
 
+
 import java.awt.*;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
 import java.net.URL;
+import java.util.Collection;
+import java.util.ArrayList;
 
 public class Plateau extends JPanel{
     //conteneur = content pane
 
     private static final long serialVersionUID = 1L; //--> qu'est ce que c'est ??
-    private Image plateauJoueur, carte;
-    private  ImageIcon gold;
+    private Image plateauJoueur;
+    private  ImageIcon gold , carte;
     private int largeurPlat;
     private int longueurPlat;
 
@@ -28,27 +34,125 @@ public class Plateau extends JPanel{
     	URL urlGold = getClass().getResource("img/gold.png");
     	
     	try {
+    		
             //la méthode statique  read de la classe javax.imageio.ImageIO renvoie une instance de la classe 
     		//BufferedImage (qui étend la classe abstraite Image).
             	
     			//Image fond
     		plateauJoueur = ImageIO.read(url7Wonders); 
-            carte = ImageIO.read(urlCarte);
             
             	//Icon
             gold = new ImageIcon(ImageIO.read(urlGold));
+            carte = new ImageIcon(ImageIO.read(urlCarte));
             
         }
         catch(IOException e){
             e.printStackTrace();
         }
     
+    	
     ///Definition LabelGold et icon
     	
-    	JLabel labelGold1 = new JLabel("111");
-        labelGold1.setIcon(gold);
+    	
+    	JLabel labelGold1 = new JLabel("111 "); //mettre espace a la fin !! car sinon trop collé
+        JLabel labelGold2 = new JLabel("22 ");
+        JLabel labelGold3 = new JLabel("333 ");
+        JLabel labelGold4 = new JLabel("444 ");
+        JLabel Esp1 = new JLabel("Esp1 "); //espace seront les endroits "vides"
+        JLabel Esp2 = new JLabel("Esp2 ");
+ 
         
-        /** test changement valeur labelgold1 :
+        Collection<JLabel> myLabelsGold = new ArrayList<JLabel>(); //création liste des labels pour chaque action soit écrite en une fois
+        //ordre important !!
+        myLabelsGold.add(labelGold1);
+        myLabelsGold.add(Esp1);
+        myLabelsGold.add(labelGold2);
+        myLabelsGold.add(labelGold3);
+        myLabelsGold.add(Esp2);
+        myLabelsGold.add(labelGold4);
+        
+        Border border = BorderFactory.createLineBorder(Color.RED , 5); //va permettre de créer des espaces pour positionner mieux
+        
+        for (JLabel label : myLabelsGold) {
+        	//border
+        	label.setBorder(border);
+        	
+        	//icon
+        	String name = label.getText(); //texte donné pas le nom du label
+        	if(!name.startsWith("Esp")) {
+        		label.setIcon(gold);
+        	}
+        	
+        	if(name.startsWith("Esp")) {
+        		label.setBorder(new EmptyBorder(10, 10, 10, 10));
+        	}
+        	
+        	//position Horizontal (tous pareil)
+        	label.setHorizontalAlignment(SwingConstants.TRAILING);
+        } 
+       
+       /** Pour le moment laisser en commentaire pour bien voir où mettre !!
+        * 
+        * for (JLabel label : myLabels) {
+        	String name = label.getText();
+        	if(name.startsWith("Esp")) {
+        		label.setBorder(new EmptyBorder(10, 10, 10, 10)); // border INVISIBLE (pas besoin si on met label en invisible)
+        		label.setVisible(false); //fonction permettant de mettre la border et jlabel invisble
+        	}
+        } */
+        
+        //position canvas (vertical uniquement voir boucle pour horizontal
+        
+        labelGold1.setVerticalAlignment(SwingConstants.TOP);
+        labelGold2.setVerticalAlignment(SwingConstants.TOP);
+        labelGold3.setVerticalAlignment(SwingConstants.CENTER);
+        labelGold4.setVerticalAlignment(SwingConstants.CENTER);
+        
+        Esp1.setVerticalAlignment(SwingConstants.TOP);
+        Esp1.setHorizontalAlignment(SwingConstants.CENTER);
+        
+       
+        
+        
+     ///Definition Label Carte
+        
+        Border borderLabelForCarte = BorderFactory.createLineBorder(Color.GREEN , 5);
+        Esp1.setLayout(new GridLayout(1,4,60,0));
+        Esp1.setBorder(borderLabelForCarte);
+        
+       
+        JLabel Carte1 = new JLabel();
+        Carte1.setIcon(carte);
+        Carte1.setVerticalAlignment(SwingConstants.BOTTOM);
+        JLabel Carte2 = new JLabel();
+        Carte2.setVerticalAlignment(SwingConstants.BOTTOM);
+        Carte2.setIcon(carte);
+        JLabel Carte3 = new JLabel();
+        Carte3.setVerticalAlignment(SwingConstants.BOTTOM);
+        Carte3.setIcon(carte);
+        JLabel Carte4 = new JLabel();
+        Carte4.setVerticalAlignment(SwingConstants.BOTTOM);
+        Carte4.setIcon(carte);
+        
+      
+        Esp1.add(Carte1);
+        Esp1.add(Carte2);
+        Esp1.add(Carte3);
+        Esp1.add(Carte4);
+       
+        
+        
+        
+        
+
+   ///mise en place Plateau
+        for (JLabel label : myLabelsGold) {
+        	this.add(label);
+        }
+        
+        
+        /** a voir lors utilsation connexion
+         *  test changement valeur labelgold1 :
          * 
         	JButton button = new JButton("Change flag");
         	button.addActionListener(new ActionListener() {
@@ -59,32 +163,6 @@ public class Plateau extends JPanel{
         });
         add(button);
         */
-        
-        JLabel  labelGold2 = new JLabel("22 ");
-        labelGold2.setIcon(gold);
-        JLabel labelGold3 = new JLabel("333");
-        labelGold3.setIcon(gold);
-        JLabel labelGold4 = new JLabel("444 ");
-        labelGold4.setIcon(gold);
-
-       
-        labelGold1.setHorizontalAlignment(SwingConstants.CENTER);
-        labelGold1.setVerticalAlignment(SwingConstants.TOP);
-
-        labelGold2.setHorizontalAlignment(SwingConstants.TRAILING);
-        labelGold2.setVerticalAlignment(SwingConstants.TOP);
-
-        labelGold3.setHorizontalAlignment(SwingConstants.CENTER);
-        labelGold3.setVerticalAlignment(SwingConstants.CENTER);
-
-        labelGold4.setHorizontalAlignment(SwingConstants.TRAILING);
-        labelGold4.setVerticalAlignment(SwingConstants.CENTER);
-
-
-        this.add(labelGold1);
-        this.add(labelGold2);
-        this.add(labelGold3);
-        this.add(labelGold4);
     }
     public void paintComponent(Graphics g) {
         
@@ -115,7 +193,7 @@ public class Plateau extends JPanel{
 
       //CARTES
          //4 en haut et 3 en bas
-        g.drawImage(carte,	largeurPlat+10,						longueurPlat+10,	largeurCarte,longueurCarte,this); 
+       /* g.drawImage(carte,	largeurPlat+10,						longueurPlat+10,	largeurCarte,longueurCarte,this); 
         g.drawImage(carte,	largeurPlat+10*2+largeurCarte,		longueurPlat+10,	largeurCarte,longueurCarte,this);
         g.drawImage(carte,	largeurPlat+15*2+largeurCarte*2,	longueurPlat+10,	largeurCarte,longueurCarte,this);
         g.drawImage(carte,	largeurPlat+15*3+largeurCarte*3,	longueurPlat+10,	largeurCarte,longueurCarte,this);
@@ -123,7 +201,7 @@ public class Plateau extends JPanel{
         g.drawImage(carte,	largeurPlat+10+largeurCarte,		longueurPlat+10+longueurCarte+10,	largeurCarte,longueurCarte,this);
         g.drawImage(carte,	largeurPlat+10*2+largeurCarte*2,	longueurPlat+10+longueurCarte+10,	largeurCarte,longueurCarte,this);
         g.drawImage(carte,	largeurPlat+15*2+largeurCarte*3,	longueurPlat+10+longueurCarte+10,	largeurCarte,longueurCarte,this);
-
+*/
     }
    
     
